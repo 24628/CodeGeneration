12d6007eda524263c24324dd4cfcb3e8f2e9e54f
Merlijn Busch <merlijn1busch@gmail.com>
2022-06-06 11:26:43 +0200

    small fixes

--- src/main/java/io/swagger/api/exceptions/GlobalExceptionHandler.java
+++ src/main/java/io/swagger/api/exceptions/GlobalExceptionHandler.java
@@ -8,67 +8,67 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 
-// throws excption with message. with the illegal argument exception class
+// returns excption with message. with the illegal argument exception class
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(IOException.class)
-    public ResponseStatusException handleUserNotFound(IOException e) {
-        throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, e.getMessage());
+    public ResponseStatusException handleIOException(IOException e) {
+        return new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseStatusException handleUserNotFound(EntityNotFoundException e) {
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+    public ResponseStatusException handleEntityNotFound(EntityNotFoundException e) {
+        return new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseStatusException handleUserNotFound(UserNotFoundException e) {
-        throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, e.getMessage());
+        return new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler(InvalidUsernameOrPassword.class)
     public ResponseStatusException handleInvalidUsernameOrPassword(InvalidUsernameOrPassword e) {
-        throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, e.getMessage());
+        return new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler(AuthorizationException.class)
     public ResponseStatusException handleAuthorization(AuthorizationException e) {
-        throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, e.getMessage());
+        return new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler(EntityAlreadyExistException.class)
-    public ResponseStatusException handleAuthorization(EntityAlreadyExistException e) {
-        throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, e.getMessage());
+    public ResponseStatusException handleEntityAlreadyExist(EntityAlreadyExistException e) {
+        return new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler(ValidationException.class)
-    public ResponseStatusException handleAuthorization(ValidationException e) {
-        throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, e.getMessage());
+    public ResponseStatusException handleValidation(ValidationException e) {
+        return new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.FORBIDDEN)
     @ExceptionHandler(InvalidPermissionsException.class)
-    public ResponseStatusException handleAuthorization(InvalidPermissionsException e) {
-        throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage());
+    public ResponseStatusException handleInvalidPermissions(InvalidPermissionsException e) {
+        return new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.FORBIDDEN)
     @ExceptionHandler(SerializeException.class)
-    public ResponseStatusException handleAuthorization(SerializeException e) {
-        throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+    public ResponseStatusException handleSerializeJson(SerializeException e) {
+        return new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(ResponseStatusException.class)
     public ResponseStatusException handleAccessDenied() {
-        throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Insufficient rights");
+        return new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Insufficient rights");
     }
 }
