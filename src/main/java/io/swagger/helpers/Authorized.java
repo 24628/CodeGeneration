a3b16a8cadd68c066724eb73c925b53448b70966
Merlijn Busch <merlijn1busch@gmail.com>
2022-06-08 02:05:47 +0200

    fix

--- src/main/java/io/swagger/helpers/Authorized.java
+++ src/main/java/io/swagger/helpers/Authorized.java
@@ -2,7 +2,9 @@ package io.swagger.helpers;
 
 import io.swagger.api.exceptions.InvalidPermissionsException;
 import io.swagger.enums.Roles;
+import io.swagger.model.Entity.AccountEntity;
 import io.swagger.model.Entity.UserEntity;
+import io.swagger.repository.IAccountDTO;
 import io.swagger.repository.IUserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,6 +19,9 @@ public class Authorized {
     @Autowired
     private IUserDTO userDTO;
 
+    @Autowired
+    private IAccountDTO accountDTO;
+
     public void NeedsToBeEmployee() throws InvalidPermissionsException {
         UserEntity user = getUser();
 
@@ -24,14 +29,24 @@ public class Authorized {
             throw new InvalidPermissionsException("no permissions to access this");
     }
 
-    public UserEntity CanOnlyEditOwnAccount(UUID uuid) throws InvalidPermissionsException {
+    public UserEntity CanOnlyEditOwnAccountByIBan(String iban) throws InvalidPermissionsException {
+        UserEntity user = getUser();
+
+        AccountEntity acc = accountDTO.getAccountByIBAN(iban);
+        UserEntity u = userDTO.findUserEntityByUuid(acc.getUserId());
+        if(!user.getRole().equals(Roles.EMPLOYEE) && !user.getUuid().equals(u.getUuid())) {
+            throw new InvalidPermissionsException("Your only allowed to view your own account");
+        }
+
+        return user;
+    }
+
+    public void CanOnlyEditOwnAccount(UUID uuid) throws InvalidPermissionsException {
         UserEntity user = getUser();
 
         if(!user.getRole().equals(Roles.EMPLOYEE) && !user.getUuid().equals(uuid)) {
             throw new InvalidPermissionsException("Your only allowed to view your own account");
         }
-
-        return user;
     }
 
     private UserEntity getUser(){
