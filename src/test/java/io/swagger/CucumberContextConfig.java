7bdecdc40399eadbf21b651facf2e06ec12acf38
Merlijn Busch <merlijn1busch@gmail.com>
2022-06-09 15:50:27 +0200

    same error after patch

---
+++ src/test/java/io/swagger/CucumberContextConfig.java
@@ -0,0 +1,11 @@
+package io.swagger;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+
+@CucumberContextConfiguration
+@ContextConfiguration
+public class CucumberContextConfig {
+}
--- src/test/java/io/swagger/steps/BaseStepDefinitions.java
+++ src/test/java/io/swagger/steps/BaseStepDefinitions.java
@@ -1,20 +1,19 @@
-package steps;
+package io.swagger.steps;
 
-import io.cucumber.spring.CucumberContextConfiguration;
+import io.swagger.CucumberContextConfig;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
-@CucumberContextConfiguration
-@SpringBootTest
-public class SpringIntegrationTest {
+@SpringBootTest(classes = CucumberContextConfig.class)
+public class BaseStepDefinitions {
 
     @LocalServerPort
     private int port;
-    // executeGet implementation
 
     private String base = "http://localhost:";
 
-    public String getBaseUrl () {
+    public String getBaseUrl() {
         return base + port;
     }
+
 }
