cc4f8284c0931fd0ca81794b7273cf3441d4da4a
TheRookieStudent <90093702+TheRookieStudent@users.noreply.github.com>
2022-06-10 13:32:00 +0200

    changes

--- src/test/java/io/swagger/steps/account/AccountStepDefs.java
+++ src/test/java/io/swagger/steps/account/AccountStepDefs.java
@@ -79,5 +79,47 @@ public class AccountStepDefs extends BaseStepDefinitions {
                 getBaseUrl() + url, HttpMethod.GET, new HttpEntity<Object>(httpHeaders),
                 String.class);
     }
+    private ResponseEntity<String> callPostHttpHeaders(String token, String url) throws JsonProcessingException {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Content-Type", "application/json");
+        httpHeaders.add("Authorization", "Bearer " + token);
+
+        return new TestRestTemplate().exchange(
+                getBaseUrl() + url, HttpMethod.POST, new HttpEntity<Object>(httpHeaders),
+                String.class);
+    }
+    private ResponseEntity<String> callPutHttpHeaders(String token, String url) throws JsonProcessingException {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Content-Type", "application/json");
+        httpHeaders.add("Authorization", "Bearer " + token);
+
+        return new TestRestTemplate().exchange(
+                getBaseUrl() + url, HttpMethod.PUT, new HttpEntity<Object>(httpHeaders),
+                String.class);
+    }
+
+
+
+    @Given("I have a valid jwt token to get accounts by iban and permissions")
+    public void iHaveAValidUserObject2() throws JSONException, JsonProcessingException {
+        getAccountsToken = getJwtToken();
+        Assertions.assertTrue(getJwtToken().startsWith("ey"));
+    }
+
+    @When("I call the account/IBAN/{IBAN} get endpoint")
+    public void iCallTheAccountGetByIbanEndpoint() throws JsonProcessingException {
+        getAccountsResponse = callGetHttpHeaders(getAccountsToken, "/accounts/iban/NL01INHO0000000001");
+    }
+
+    @And("I get an list of account back (saving or normal)")
+    public void iGetAListOfAccountsBackByIban() throws JSONException {
+        JSONObject jsonObject = new JSONObject(getAccountsResponse.getBody());
+        String accountEntityList = jsonObject.getString("accountEntityList");
+        System.out.println(accountEntityList);
+
+        Assertions.assertTrue(accountEntityList.contains("iban"));
+        Assertions.assertTrue(accountEntityList.contains("uuid"));
+    }
+
 
 }
--- src/test/resources/account.feature
+++ src/test/resources/account.feature
@@ -6,3 +6,32 @@ Feature: the account controller works
     Then I receive a status of success of 200
     And I get a list of accounts back
 
+  Scenario: I can change the limits (accountLimit/absoluteLimit) account data of the user
+    Given I have a valid jwt token to get accounts by IBAN and permissions
+    When I call the account/IBAN/{IBAN} post endpoint
+    Then I receive a status of success of 200
+    And I change the account data of the user
+
+  Scenario: I can change the limits (accountLimit/absoluteLimit) account data of the user
+    Given I have a valid jwt token to get accounts by IBAN and permissions
+    When I call the account/IBAN/{IBAN} put endpoint
+    Then I receive a status of success of 200
+    And I change the account data of the user
+
+  Scenario: I can get list of account based on the userId
+    Given I have a valid jwt token to get accounts by userId and permissions
+    When I call the account/id/{id} get endpoint
+    Then I receive a status of success of 200
+    And I get an list of account back (saving or normal) by userId
+
+
+  Scenario: I can get single of account based on the IBAN
+    Given I have a valid jwt token to get accounts by IBAN and permissions
+    When I call the account/IBAN/{IBAN} get endpoint
+    Then I receive a status of success of 200
+    And I get a single account using the IBAN
+
+
+
+
+
